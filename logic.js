1ebaa3595f619afad3d111a07c42d5dfa25e40e9
Moeez Rajpoot <99912138+Moeez-Rajpoot@users.noreply.github.com>
2023-08-11 15:33:01 +0500

    Updated

---
+++ logic.js
@@ -0,0 +1,22 @@
+const selectbar = document.getElementById('select');
+const option = document.getElementsByTagName('li');
+const optiondiv = document.getElementById('list');
+const selectbartxt = document.getElementById('value'); 
+const img = document.getElementById('arrow');
+
+selectbar.addEventListener('click', () => {
+  optiondiv.classList.toggle('hide');
+  img.classList.toggle('rotate');
+});
+
+const optionArray = Array.from(option);
+
+optionArray.forEach(item => {
+  item.addEventListener('click', function(event) {
+    const clickedSpan = event.currentTarget.querySelector('span');
+    selectbartxt.innerText = clickedSpan.textContent;
+    optiondiv.classList.toggle('hide');
+  img.classList.toggle('rotate');
+
+  });
+});
